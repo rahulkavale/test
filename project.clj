1371361bbe10f3ff7ffcfede8036a66154d99234
Rahul Kavale <rahulkavale@Rahuls-MacBook-Pro.local>
2016-06-10 05:38:58 +0530

    Added dependecy for jdbc and ring-json

--- project.clj
+++ project.clj
@@ -4,7 +4,11 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [compojure "1.4.0"]
-                 [ring/ring-defaults "0.1.5"]]
+                 [ring/ring-defaults "0.1.5"]
+                 [org.clojure/java.jdbc "0.4.1"]
+                 [org.postgresql/postgresql "9.4-1201-jdbc41"]
+                 [ring/ring-jetty-adapter "1.4.0"]
+                 [ring/ring-json "0.1.2"]]
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler mytweeter.handler/app}
   :profiles
