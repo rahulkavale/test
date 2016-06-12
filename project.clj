be23279eec878a2484450cb4fffa45113428e250
Rahul Kavale <rahulkavale@Rahuls-MacBook-Pro.local>
2016-06-12 18:26:43 +0530

    Added cheshire for json dependency

--- project.clj
+++ project.clj
@@ -3,12 +3,13 @@
   :url "http://example.com/FIXME"
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.7.0"]
-                 [compojure "1.4.0"]
+                 [compojure "1.5.0"]
                  [ring/ring-defaults "0.1.5"]
                  [org.clojure/java.jdbc "0.4.1"]
                  [org.postgresql/postgresql "9.4-1201-jdbc41"]
                  [ring/ring-jetty-adapter "1.4.0"]
-                 [ring/ring-json "0.1.2"]]
+                 [ring/ring-json "0.4.0"]
+                 [cheshire "5.6.1"]]
   :main mytweeter.handler
   :plugins [[lein-ring "0.9.7"]]
   :ring {:handler mytweeter.handler/app}
